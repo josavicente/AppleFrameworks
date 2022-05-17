47dd04f5261502d8319556bd19099ff8b49b81c3
Josa Vicente <jvicenpe@gmail.com>
2022-05-17 23:43:02 +0200

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Josafat Vicente Pérez on 17/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
