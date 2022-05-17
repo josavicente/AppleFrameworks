0cc1e9079c16f0044eab26d58ae453f8c6a4ae19
Josa Vicente <jvicenpe@gmail.com>
2022-05-17 23:53:59 +0200

    v1.0

---
+++ AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Josafat Vicente Pérez on 17/5/22.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
