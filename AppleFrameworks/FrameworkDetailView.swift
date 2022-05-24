5c561bfe60fe7ca6ba2494eb47bb9cc62166a29b
Josa Vicente <jvicenpe@gmail.com>
2022-05-24 20:37:33 +0200

    v1.0

--- AppleFrameworks/FrameworkDetailView.swift
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -11,7 +11,7 @@ struct FrameworkDetailView: View {
     
     var framework: Framework
     @Binding var isShowingDetailView: Bool
-    
+    @State private var isShowingSafariView = false
     var body: some View {
         VStack{
             HStack{
@@ -32,11 +32,13 @@ struct FrameworkDetailView: View {
                 .padding()
             Spacer()
             Button {
-                print("Press")
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
-            
+            .sheet(isPresented: $isShowingSafariView, content: {
+                SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+            })
         }.padding()
     }
 }
---
+++ AppleFrameworks/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  AppleFrameworks
+//
+//  Created by Josafat Vicente Pérez on 24/5/22.
+//
+
+import Foundation
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
